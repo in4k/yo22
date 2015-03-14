9fbd22474b24e1b42cf8aae6c6fec6bb065bbb95
Ivan 'w23' Avdeev <me@w23.ru>
2015-03-15 03:26:02 +0600

    yo22: update shaders, check windows build

--- make_shaders.sh
+++ make_shaders.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 function compose() {
-	echo "static const char *fragment_shaders[Prog_COUNT+1] = {"
+	echo "static const char *fragment_shaders[Prog_COUNT] = {"
 	for f; do
 		cat $f | sed \
+			-e 's/\/\/.*//' \
 			-e 's/\\/\\\\/g' \
 			-e 's/^\s*//' \
 			-e 's/\s\{2,\}//g' \
-			-e 's/\s*\([-=,;()]\)\s*/\1/' \
+			-e 's/\s*\([{}();*+=,.-]\+\)\s*/\1/g' \
 			-e 's/^/"/' \
 			-e 's/$/\\n"/'
 
 			#-e ':a;N;$!ba;s/\n/ /g' \
 		echo ", "
 	done
-	echo "\"\"};"
+	echo "};"
 }
 
-compose generator.glsl eroder.glsl tracer.glsl postprocessor.glsl > shaders.h
+compose generator.glsl eroder.glsl planner.glsl tracer.glsl postprocessor.glsl > shaders.h
